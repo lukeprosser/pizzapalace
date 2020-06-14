20a4ee5812c43ebcaafaa44c0b8b696e9bc8c649
Luke Prosser <lukesprosser@gmail.com>
2020-06-14 18:38:28 +0100

    Front page block testing

--- front-page.php
+++ front-page.php
@@ -1,16 +1,31 @@
 <?php get_header(); ?>
 
 <main class="site-content" role="main">
-
+  
   <?php get_template_part('home/intro') ?>
 
-  <?php get_template_part('home/about') ?>
+  <?php // get_template_part('home/about') ?>
 
-  <?php get_template_part('home/gallery') ?>
+  <?php 
 
-  <?php get_template_part('home/recent-posts') ?>
+    if (have_posts()) : 
+      while (have_posts()) : the_post();
+        
+        the_content();
+        
+      endwhile;
+
+    else :
+      echo '<p>No content found</p>';
+    endif;
+
+  ?>
+
+  <?php // get_template_part('home/gallery') ?>
+
+  <?php // get_template_part('home/recent-posts') ?>
     
-  <?php get_template_part('home/contact') ?>
+  <?php // get_template_part('home/contact') ?>
 
 </main><!-- .site-content -->
 
